ce6a826a44e6f4cc83f4381e4f502857d5d4f1d2
MarbleGate <marblegatekeeper@outlook.com>
2025-04-19 08:40:36 +0800

    [update] Fishing Net captures small aquatic animal

--- src/main/java/plus/dragons/createintegratedfarming/common/fishing/net/FishingNetMovementBehaviour.java
+++ src/main/java/plus/dragons/createintegratedfarming/common/fishing/net/FishingNetMovementBehaviour.java
@@ -18,17 +18,35 @@
 
 package plus.dragons.createintegratedfarming.common.fishing.net;
 
+import com.simibubi.create.AllItems;
 import com.simibubi.create.api.behaviour.movement.MovementBehaviour;
 import com.simibubi.create.content.contraptions.behaviour.MovementContext;
 import java.util.List;
+
+import com.simibubi.create.content.kinetics.saw.SawBlock;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.DamageTypes;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.animal.WaterAnimal;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.storage.loot.BuiltInLootTables;
+import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
 import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.EventHooks;
 import net.neoforged.neoforge.event.entity.player.ItemFishedEvent;
+import plus.dragons.createintegratedfarming.config.CIFConfig;
 
 public class FishingNetMovementBehaviour implements MovementBehaviour {
     @Override
@@ -37,6 +55,37 @@ public class FishingNetMovementBehaviour implements MovementBehaviour {
             var fishing = getFishingNetContext(context, level);
             if (fishing.timeUntilCatch > 0)
                 fishing.timeUntilCatch--;
+            if (level.getGameTime() % 20 == 0 && CIFConfig.server().fishingNetCaptureSmallLivingBeingInWater.get()){
+                AABB effectiveArea = context.state.getShape(level,context.localPos).bounds().expandTowards(context.motion.multiply(5,5,5)).move(context.position).inflate(0.2);
+                var entities = level.getEntities((Entity) null, effectiveArea, entity -> {
+                    if(entity instanceof WaterAnimal animal){ // QUESTION: Should we support all mobs?
+                        return animal.getBbWidth() <= CIFConfig.server().fishingNetCapturedLivingBeingMaxSize.get() &&
+                                animal.getBbHeight() <= CIFConfig.server().fishingNetCapturedLivingBeingMaxSize.get();
+                    } return false;
+                });
+                entities.forEach(entity -> {
+                    var animal = (WaterAnimal) entity;
+                    if (!animal.isBaby() && level.getGameRules().getBoolean(GameRules.RULE_DOMOBLOOT)) {
+                        var damageSource = new DamageSource(level.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.GENERIC_KILL),context.position);
+                        var lootTable = level.getServer().reloadableRegistries().getLootTable(animal.getLootTable());
+                        var lootParams = (new LootParams.Builder(level))
+                                .withParameter(LootContextParams.THIS_ENTITY, animal)
+                                .withParameter(LootContextParams.ORIGIN, animal.position())
+                                .withParameter(LootContextParams.DAMAGE_SOURCE, damageSource)
+                                .withOptionalParameter(LootContextParams.ATTACKING_ENTITY, context.contraption.entity)
+                                .withOptionalParameter(LootContextParams.DIRECT_ATTACKING_ENTITY, context.contraption.entity)
+                                .withLuck(EnchantmentHelper.getFishingLuckBonus(level, getFishingNetContext(context, level).fishingRod, context.contraption.entity));
+                        lootTable.getRandomItems(lootParams.create(LootContextParamSets.ENTITY), animal.getLootTableSeed(), item -> dropItem(context,item));
+                        if(CIFConfig.server().fishingNetCapturedLivingBeingDropExpNugget.get()){
+                            int reward = EventHooks.getExperienceDrop(animal, null, animal.getExperienceReward(level, entity));
+                            int count = reward / 3;
+                            if (reward > 0 && count == 0) count = 1;
+                            dropItem(context, new ItemStack(AllItems.EXP_NUGGET.get(), count));
+                        }
+                        animal.discard();
+                    }
+                });
+            }
         }
     }
 
--- src/main/java/plus/dragons/createintegratedfarming/config/CIFServerConfig.java
+++ src/main/java/plus/dragons/createintegratedfarming/config/CIFServerConfig.java
@@ -36,7 +36,15 @@ public class CIFServerConfig extends ConfigBase {
     public final ConfigInt fishingNetMaxRecordedBlocks = i(8, 1, 64,
             "fishingNetMaxRecordedBlocks",
             Comments.fishingNetMaxRecordedBlocks);
-
+    public final ConfigBool fishingNetCaptureSmallLivingBeingInWater = b(true,
+            "fishingNetCaptureSmallLivingBeingInWater",
+            Comments.fishingNetCaptureSmallLivingBeingInWater);
+    public final ConfigFloat fishingNetCapturedLivingBeingMaxSize = f(0.7f, 0.01f, 10f,
+            "fishingNetCapturedLivingBeingMaxSize",
+            Comments.fishingNetCapturedLivingBeingMaxSize);
+    public final ConfigBool fishingNetCapturedLivingBeingDropExpNugget = b(true,
+            "fishingNetCapturedLivingBeingDropExpNugget",
+            Comments.fishingNetCapturedLivingBeingDropExpNugget);
     public final ConfigGroup ranching = group(1, "ranching", Comments.ranching);
     public final ConfigBool leashedEntitySitsAutomatically = b(false,
             "leashedEntitySitsAutomatically",
@@ -66,7 +74,16 @@ public class CIFServerConfig extends ConfigBase {
                 "Increasing this value will reduce the efficiency of Fishing Net that travels in a fixed short route.",
                 "Example: Fishing Net placed near the rotating axis."
         };
-
+        static final String[] fishingNetCaptureSmallLivingBeingInWater = {
+                "If Fishing Net should capture small size animals and automatically process them.",
+                "\"Process\" means captured entity will be discard and all drops will be collected."
+        };
+        static final String[] fishingNetCapturedLivingBeingMaxSize = {
+                "The maximum size (width and height) of aquatic animal that can be caught by the Fishing Net."
+        };
+        static final String[] fishingNetCapturedLivingBeingDropExpNugget = {
+                "If animals captured by Fishing Net should drop Nugget of Experience."
+        };
         static final String ranching = "Settings for Ranching utilities";
         static final String[] leashedEntitySitsAutomatically = {
                 "If leashed entity automatically sits on unoccupied seat.",
