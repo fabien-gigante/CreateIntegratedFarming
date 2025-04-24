51c6daa37405a8af8ea26db678a58e8e331434a2
MarbleGate <marblegatekeeper@outlook.com>
2025-04-24 16:22:30 +0800

    [update] Roost can now catch chicken from spawner

--- src/main/java/plus/dragons/createintegratedfarming/common/ranching/roost/RoostBlockItem.java
+++ src/main/java/plus/dragons/createintegratedfarming/common/ranching/roost/RoostBlockItem.java
@@ -18,13 +18,33 @@
 
 package plus.dragons.createintegratedfarming.common.ranching.roost;
 
+import com.simibubi.create.AllBlocks;
+import com.simibubi.create.AllTags;
+import net.createmod.catnip.math.VecHelper;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.random.WeightedEntry;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.BaseSpawner;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.SpawnData;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.SpawnerBlockEntity;
+import net.minecraft.world.phys.Vec3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class RoostBlockItem extends BlockItem {
     public RoostBlockItem(Block block, Properties properties) {
@@ -35,5 +55,33 @@ public class RoostBlockItem extends BlockItem {
     public InteractionResult interactLivingEntity(ItemStack stack, Player player, LivingEntity target, InteractionHand hand) {
         var capturable = RoostCapturable.REGISTRY.get(target.getType());
         return capturable == null ? InteractionResult.PASS : capturable.captureItem(target.level(), stack, hand, player, target);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext context) {
+        Level world = context.getLevel();
+        BlockPos pos = context.getClickedPos();
+        BlockEntity be = world.getBlockEntity(pos);
+        Player player = context.getPlayer();
+        if (!(be instanceof SpawnerBlockEntity))
+            return super.useOn(context);
+        BaseSpawner spawner = ((SpawnerBlockEntity) be).getSpawner();
+        List<SpawnData> possibleSpawns = spawner.spawnPotentials.unwrap()
+                .stream()
+                .map(WeightedEntry.Wrapper::data)
+                .toList();
+        if (possibleSpawns.isEmpty()) {
+            possibleSpawns = new ArrayList<>();
+            possibleSpawns.add(spawner.nextSpawnData);
+        }
+        for (SpawnData e : possibleSpawns) {
+            Optional<EntityType<?>> optionalEntity = EntityType.by(e.entityToSpawn());
+            if (optionalEntity.isEmpty())
+                continue;
+            var capturable = RoostCapturable.REGISTRY.get(optionalEntity.get());
+            if (capturable == null) continue;
+            return capturable.captureItem(world, player.getItemInHand(context.getHand()), context.getHand(), player, optionalEntity.get().create(world));
+        }
+        return super.useOn(context);
     }
 }
